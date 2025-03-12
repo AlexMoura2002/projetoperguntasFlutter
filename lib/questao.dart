24f2819e85799f71ff55864ec49ad67db99dc8be
Alex Moura da Silva <222.14.061@EDU.gruposete.local>
2025-03-12 19:28:34 -0300

    Primeiro commit

---
+++ lib/questao.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Questao extends StatelessWidget {
+  final String texto;
+
+  Questao(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        texto,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
